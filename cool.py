86bda2743934e45203c62aee0526f8352e7f9dd8
MAHIN-71 <106368965+MAHIN-71@users.noreply.github.com>
2022-10-01 14:10:33 +0600

    Update cool.py

--- cool.py
+++ cool.py
@@ -1,1 +1,21 @@
-import cool
+import platform
+import os
+try:os.system("xdg-open https://youtube.com/channel/UC6kZyTFgaF19CbWtKCSbt2g")
+except:pass
+try:os.system("clear")
+except:pass
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:                                                                                __import__("latter")._site_view_()
+elif 'aarch' in arc:
+        __import__("cool").Main()
+else:                                                                                           exit(f' Unknow device machine {arc}')
