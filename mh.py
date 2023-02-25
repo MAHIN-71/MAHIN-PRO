80a477096e19b5653e9dd6a833d923b24b77cfb4
MAHIN-71 <106368965+MAHIN-71@users.noreply.github.com>
2023-02-25 20:06:22 +0600

    Update mh.py

--- mh.py
+++ mh.py
@@ -1,22 +1,4 @@
 import platform
 import os
 try:os.system("xdg-open https://chat.whatsapp.com/Iwto8kUxcbFK4EuifbillQ")
-except:pass
-try:os.system("clear")
-except:pass
-os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('touch .prox.txt')
-except:pass
-try:os.system('touch .proxy.txt')
-except:pass
-try:os.mkdir('/sdcard/OK')
-except:pass
-try:os.mkdir('/sdcard/CP')
-except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-        __import__("latter")._site_view_()
-elif 'aarch' in arc:
-        __import__("mah").menu()
-else:                                                                                                        exit(f' Unknow device machine {arc}')
+import mahin
