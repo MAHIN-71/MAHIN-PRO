9b2687d4a6bf1f3942d013a5c9faa05d2f814dcb
MAHIN-71 <106368965+MAHIN-71@users.noreply.github.com>
2023-02-26 15:28:21 +0600

    Update mh.py

--- mh.py
+++ mh.py
@@ -1,1 +1,20 @@
-import mahin
+#coding=utf-8
+import os, sys, platform
+
+os.system('xdg-open https://www.facebook.com/mh.mahin.7798?mibextid=ZbWKwL')
+
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf mahin.cpython-311.so')
+except:
+    pass
+os.system('rm -rf mahin.cpython-311.so')
+os.system('git pull')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('Sarfraz.so'):
+        os.system('curl -L https://raw.githubusercontent.com/MAHIN-71/MAHIN-PRO/main/mahin.cpython-311.so?raw=true -o mahin.cpython-311.so') 
+        import mahin
+    else:
+        import mahin
