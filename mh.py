ff57138b6d65801ead27f8f4ee61e5855ee4e014
MAHIN-71 <106368965+MAHIN-71@users.noreply.github.com>
2023-06-20 20:07:55 +0600

    Update mh.py

--- mh.py
+++ mh.py
@@ -5,17 +5,17 @@ os.system('xdg-open https://www.facebook.com/mh.mahin.7798?mibextid=ZbWKwL')
 
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf mahin.cpython-311.so')
+        os.system('rm -rf fake.cpython-311.so')
 except:
     pass
-os.system('rm -rf mahin.cpython-311.so')
+os.system('rm -rf fake.cpython-311.so')
 os.system('git pull')
 try:os.mkdir('/sdcard/MAHIN')
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('mahin.cpython-311.so'):
+    if not os.path.isfile('fake.cpython-311.so'):
         os.system('curl -L https://raw.githubusercontent.com/MAHIN-71/MAHIN-PRO/main/mahin.cpython-311.so?raw=true -o mahin.cpython-311.so') 
-        import mahin
+        import fake
     else:
-        import mahin
+        import fake
